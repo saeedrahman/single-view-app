39ef731ffc36d26527404e4fac615922c4927423
Saeed Rahman <saeedrahman@cdnis.edu.hk>
2020-01-06 13:02:54 +0800

    Initial Commit

---
+++ SwiftSingleViewApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftSingleViewApp
+//
+//  Created by Saeed Rahman on 6/1/2020.
+//  Copyright © 2020 Saeed Rahman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
